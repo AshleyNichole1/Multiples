d4ef4bcccef0e356e10ed7afb3958954d4aa577c
AshleyNichole1 <ashleynicholelea@outlook.com>
2021-10-04 16:34:45 -0500

    initial commit plus multiples coding challenge

--- Multiples3and5/Program.cs
+++ Multiples3and5/Program.cs
@@ -10,6 +10,24 @@ namespace Multiples3and5
     {
         static void Main(string[] args)
         {
+            int threes = 0;
+            //multiples of 3 
+            for (int i = 3; i <= 1000; i += 3)
+            {
+                threes += i;
+            }
+
+            Console.WriteLine($"Sum of the multiples of three: {threes}\n");
+            //multiples of 5 
+            int five = 0;
+            for (int i = 5; i <= 1000; i += 5)
+            {
+                five += i;   
+            }
+            Console.WriteLine($"Sum of the multiples of five: {five}\n");
+
+            //find the sum of all of the multiples
+            Console.WriteLine($"Sum of the multiples of three and five: {five + threes}");
         }
     }
 }
